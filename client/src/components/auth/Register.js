039b7ca85cbbcf66e6a1aead8a1343db72452b18
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-08-17 17:17:54 +0530

    Made some changes

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -31,17 +31,17 @@ const Register = (props) => {
         <div>
             <form onSubmit={onSubmit}>
                 <label>Name</label>
-                <input type='text' name='name' value={name} onChange={onFormChange}></input>
+                <input type='text' name='name' value={name} onChange={onFormChange}></input><br />
                 <label>Email</label>
-                <input type='email' name='email' value={email} onChange={onFormChange}></input>
+                <input type='email' name='email' value={email} onChange={onFormChange}></input><br />
                 <label>Password</label>
-                <input type='password' name='password1' minLength='6' value={password1} onChange={onFormChange}></input>
+                <input type='password' name='password1' minLength='6' value={password1} onChange={onFormChange}></input><br />
                 <labe>Confirm password</labe>
-                <input type='password' name='password2' minLength='6' value={password2} onChange={onFormChange}></input>
+                <input type='password' name='password2' minLength='6' value={password2} onChange={onFormChange}></input><br />
                 <input type='submit' vlaue='Submit' />
             </form>
         </div>
     )
 }
-
+ 
 export default Register
--- routes/auth.js
+++ routes/auth.js
@@ -18,4 +18,47 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
+router.post('/',
+    [
+        check('email', 'Please enter an email').not().isEmpty(),
+        check('password', 'Please enter a password').exists()
+    ],
+    async (req, res) => {
+        const error = validationResult(req);
+        if(!error.isEmpty()) {
+            return res.status(400).json({ message : error.array() });
+        }
+
+        const { email, password } = req.body;
+        try {
+            let user = User.findOne({ email });
+            if(!user) {
+                res.status(400).send('Invalid credentials');
+            }
+            const correctPassword = await bcrypt.compare(password, user.password);
+            if(!correctPassword) {
+                res.status(400).send('Incorrect password');
+            }
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSeceret'),
+                { expiresIn: 3600000 },
+                (err, token) => {
+                    if(err) throw err;
+                    res.status(200).json({ token });
+                }
+            );
+        } catch(error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+)
+
 module.exports = router;
