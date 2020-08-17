a446ea61016537b7d1f5c2b2c4cc1daf63fa7f12
Ujjwal-Shekhawat <ujjus2001@gmail.com>
2020-08-17 16:03:07 +0530

    Working on frontend Reducers and Contexts

---
+++ client/src/context/auth/authState.js
@@ -0,0 +1,8 @@
+//Reducer function
+export default (state, action) => {
+    switch(action.type) {
+        case 'reg_success':
+        case 'login_success': // I still have to implement the login auth
+            localStorage.setItem('token', action.payload.token);
+    }
+}
